6fd90311add6e133f0b5fc63f35c3d10c1ca3ce4
vishalw3villa <vishalprakash.maurya@w3vila.com>
2023-07-21 18:28:07 +0530

    solve bug

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let display = document.getElementById("digital_input");
 let operator = "";
 let check = false; /*  For error and after erasing it */
 let dot = false; /*For not showing double dot into expression in one operand */
+let pressedEqual = false;
 
 btn.addEventListener('click', handleKey)
 
@@ -18,6 +19,7 @@ function handleKey(e) {
             display.value = "";
         }
         else if (action == "equal") {
+            check = true;
             dot = false;
             try {
                 let result = eval(display.value);
@@ -36,8 +38,9 @@ function handleKey(e) {
                     display.value = "error";
                 }
 
-                check = true;
             }
+            pressedEqual = true;
+            console.log(pressedEqual)
         }
         else if (action == "delete") {
             // display.value = display.value.slice(0, display.value.length-1);
@@ -56,22 +59,45 @@ function handleKey(e) {
         else {
 
             if ((!action && display.value == "") || (check && !action)) {
+                // console.log(check);
+                // console.log(pressedEqual);
+                console.log("Hello1");
+
+                if ((pressedEqual && !action)) {
+                    display.value = "";
+                    pressedEqual = false;
+                    check = false;
+                }
+
+
+
                 display.value = e.target.innerText;
                 if (check) {
                     check = false;
                 }
             }
             else if ((action && display.value == "") || (check && action)) {
-
-                if (action == "subtract") {
-                    display.value += e.target.innerText;
+                console.log("Hello2");
+                console.log(check);
+                console.log(pressedEqual)
+                if (pressedEqual && action) {
+                    pressedEqual = false;
+                    check = false;
                 }
 
-                if (action == "dot") {
+                if (display.value == "" && action == "subtract") {
+                    display.value += e.target.innerText;
+                }
+                else if (display.value == "" && action == "dot") {
                     display.value += 0;
                     display.value += e.target.innerText;
                     dot = true;
                 }
+                else if(display.value != ""){
+                    display.value += e.target.innerText;
+                }
+
+
             }
             else if (action && isOperator(display.value.slice(-1))) {
                 if (action == "dot") {
@@ -117,6 +143,4 @@ function isOperator(opr) {
         return false;
     }
 }
-
-
 
